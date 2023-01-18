9773ecccd3a4ccd34a3421e42d0f9a8e524feafe
Zain <muhzainjaved@gmail.com>
2023-01-18 16:05:08 +0500

    Registration and Login API's converted on GraphQL standards

--- authenticate_user/schema.py
+++ authenticate_user/schema.py
@@ -5,18 +5,21 @@ from oauth2_provider.decorators import protected_resource
 from django.contrib.auth import authenticate
 from oauth2_provider.models import AccessToken as Token
 
+import graphene
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 class UserType(DjangoObjectType):
     class Meta:
         model = User
 
-class Query(graphene.ObjectType):
-    me = graphene.Field(UserType)
+# class Query(graphene.ObjectType):
+#     me = graphene.Field(UserType)
 
-    @protected_resource()
-    def resolve_me(self, info):
-        user = info.context.user
-        return user
+#     @protected_resource()
+#     def resolve_me(self, info):
+#         user = info.context.user
+#         return user
 
 class CreateUser(graphene.Mutation):
     user = graphene.Field(lambda: UserType)
@@ -49,8 +52,54 @@ class Login(graphene.Mutation):
         token, created = Token.objects.get_or_create(user=user)
         return Login(token=token.key)
 
+# class Mutation(graphene.ObjectType):
+#     create_user = CreateUser.Field()
+#     login = Login.Field()
+
+# schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
+class RegisterMutation(graphene.Mutation):
+    class Arguments:
+        username = graphene.String()
+        email = graphene.String()
+        password = graphene.String()
+
+    user = graphene.Field(lambda: UserType)
+
+    def mutate(self, info, username, email, password):
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return RegisterMutation(user=user)
+
+class LoginMutation(graphene.Mutation):
+    class Arguments:
+        username = graphene.String()
+        password = graphene.String()
+
+    user = graphene.Field(lambda: UserType)
+
+    def mutate(self, info, username, password):
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(info.context, user)
+            return LoginMutation(user=user)
+        else:
+            raise Exception("Invalid credentials")
+
+class Query(graphene.ObjectType):
+    me = graphene.Field(UserType)
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Not logged in")
+        return user
+
 class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
-    login = Login.Field()
+    register = RegisterMutation.Field()
+    login = LoginMutation.Field()
+
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
